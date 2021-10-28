7eb16e4dd7962c8a8e3862675a66e65246139d07
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2021-10-28 14:16:40 -0600

    add ps aux output

--- system76driver/util.py
+++ system76driver/util.py
@@ -62,6 +62,7 @@ def dump_logs(base):
     dump_command(base, "journalctl", ["journalctl", "--since", "yesterday"])
     dump_command(base, "sensors", ["sensors"])
     dump_command(base, "uptime", ["uptime"])
+    dump_command(base, "ps aux", ["ps aux"])
     dump_path(base, "fstab", "/etc/fstab")
     dump_path(base, "apt/sources.list", "/etc/apt/sources.list")
     dump_path(base, "apt/sources.list.d", "/etc/apt/sources.list.d")
